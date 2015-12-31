6452e1296133384b9ea1ef045e536b60abf93cd1
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-31 12:35:50 +0100

    Migrate to version 3.8 (grafted from fbf97f5fe8fe5b1a21f7c45338a9fb3f3d47f692)

--- tests/test_galatea_blog.py
+++ tests/test_galatea_blog.py
@@ -4,22 +4,12 @@
 # the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GalateaBlogTestCase(unittest.TestCase):
+class GalateaBlogTestCase(ModuleTestCase):
     'Test Galatea Blog module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('galatea_blog')
-
-    def test0005views(self):
-        'Test views'
-        test_view('galatea_blog')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'galatea_blog'
 
 
 def suite():
